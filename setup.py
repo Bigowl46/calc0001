3c024d6851569358fd63a432d4683981e1577b1b
Bigowl46 <162390012+Bigowl46@users.noreply.github.com>
2024-04-03 14:41:57 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup (
+  include_package_data = True, 
+  name='mycalc0001',
+  version='0.0.1',
+  description='oss-dev my calculator 0001',
+  author='sejinchun',
+  author_email='sjchun@dau.ac.kr',
+  install_requires=['pytest'],
+  long_description = 'oss-dev calculator python module',
+  long_description_content_type = 'text/markdown',
+  classifiers=[
+      "Programmming Language :: Python :: 3",
+      "Operating System :: OS Independent",
+  ],
+)
+  
